cb5570b122621c1d5b292d81f9cf00e17304c181
towa1204 <fdsfgjkal@gmail.com>
2020-11-18 23:47:07 +0900

    単一RSブロックにおける既存手法の誤り訂正能力の実装，ファイル書き込み機能追加

--- outputECC/src/outputECC/OutputECC.java
+++ outputECC/src/outputECC/OutputECC.java
@@ -1,5 +1,9 @@
 package outputECC;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import com.google.zxing.qrcode.decoder.Decoder;
@@ -17,45 +21,115 @@ public class OutputECC {
        ErrorCorrectionLevel.Q, ErrorCorrectionLevel.H};
 
     // 型番
-    int versionNumber = 3;
+    int versionNumber = 6;
     // 誤り訂正レベル
-    ErrorCorrectionLevel ecLevelVal = ecLevel[0];
-    // 埋め込む情報k'
-    int kp  = 10;
+    ErrorCorrectionLevel ecLevelVal = ecLevel[3];
+
+    String fileName = "C:\\Research2020\\evaluation\\evaluation_2_6H.csv";
 
     Version version = Version.getVersionForNumber(versionNumber);
     Version.ECBlocks ecBlocks = version.getECBlocksForLevel(ecLevelVal);
     int maxNumDataBytes = version.getTotalCodewords() - ecBlocks.getTotalECCodewords();
     int numTotalCodewords = version.getTotalCodewords();
 
-    NewVersion newVersion = new NewVersion(versionNumber, ecLevelVal, kp);
-    NewVersion.NewECBlocks newECBlocks = newVersion.getECBlocks();
-    NewVersion.NewECB[] ecb = newECBlocks.getECBlocks();
+    // この型番・誤り訂正レベルのとき単一RSブロックか否か
+    boolean flag = isSingleRSBlock(ecBlocks);
+    double[][] eccArray = new double[flag ? 4 : 3][maxNumDataBytes];
 
-    // debug 用
-    System.out.println("従来の手法の誤り訂正能力");
-    double normalECC = calcNormalECC(ecBlocks, numTotalCodewords);
-    System.out.println("normalECC = " + normalECC);
-    System.out.println();
+    // 小数点第2位以下は切り捨て
+    for (int i = 1; i <= maxNumDataBytes; i++) {
+      NewVersion newVersion = new NewVersion(versionNumber, ecLevelVal, i);
+      NewVersion.NewECBlocks newECBlocks = newVersion.getECBlocks();
+      NewVersion.NewECB[] ecb = newECBlocks.getECBlocks();
 
-    if (isSingleRSBlock(ecBlocks)) {
-      System.out.println("既存手法(単一RSブロック)の誤り訂正能力");
-      double exsistSingleECC = calcExsistSingleECC(kp, numTotalCodewords);
-      System.out.println("exsistSingleECC = " + exsistSingleECC);
-      System.out.println();
+      // debug 用
+//      System.out.println("k' = " + i);
+//      System.out.println("提案手法の誤り訂正能力");
+//      double proposedECC = calcProposedECC(ecb, numTotalCodewords);
+//      System.out.println("proposedECC = " + Math.floor(proposedECC * 10) / 10);
+//      System.out.println();
+//
+//      System.out.println("従来の手法の誤り訂正能力");
+//      double normalECC = calcNormalECC(ecBlocks, numTotalCodewords);
+//      System.out.println("normalECC = " + Math.floor(normalECC * 10) / 10);
+//      System.out.println();
+//
+//      if(flag) {
+//        System.out.println("既存手法(単一RSブロック)の誤り訂正能力");
+//        double exsistSingleECC = calcExsistSingleECC(i, numTotalCodewords);
+//        System.out.println("exsistSingleECC = " + Math.floor(exsistSingleECC * 10) / 10);
+//        System.out.println();
+//      }
+//
+//      System.out.println("既存手法(複数RSブロック)の誤り訂正能力");
+//      double exsistMultiECCsub = calcExsistMultipleECC(maxNumDataBytes, i, ecBlocks);
+//      double exsistMultiECC = exsistMultiECCsub != -1 ? exsistMultiECCsub : normalECC;
+//      System.out.println("exsistMultiECC = " + Math.floor(exsistMultiECC * 10) / 10);
+//      System.out.println();
+
+
+      double proposedECC = calcProposedECC(ecb, numTotalCodewords);
+      eccArray[0][i-1] = Math.floor(proposedECC * 10) / 10;
+      double normalECC = calcNormalECC(ecBlocks, numTotalCodewords);
+      eccArray[1][i-1] = Math.floor(normalECC * 10) / 10;
+
+      double exsistMultiECCsub = calcExsistMultipleECC(maxNumDataBytes, i, ecBlocks);
+      double exsistMultiECC = exsistMultiECCsub != -1 ? exsistMultiECCsub : normalECC;
+      /*
+       * この実験は誤り訂正能力の高さを評価するものだから、
+       * 別の符号語よりも通常の符号語の方が高くなった場合は、そっちの方へ値を変えるようにする
+       * */
+      if (normalECC > exsistMultiECC) {
+        exsistMultiECC = normalECC;
+      }
+
+      if(flag) {
+        double exsistSingleECC = calcExsistSingleECC(i, numTotalCodewords);
+        eccArray[2][i-1] = Math.floor(exsistSingleECC * 10) / 10;
+        eccArray[3][i-1] = Math.floor(exsistMultiECC * 10) / 10;
+      } else {
+        eccArray[2][i-1] = Math.floor(exsistMultiECC * 10) / 10;
+      }
     }
 
-    System.out.println("既存手法(複数RSブロック)の誤り訂正能力");
-    double exsistMultiECCsub = calcExsistMultipleECC(maxNumDataBytes, kp, ecBlocks);
-    double exsistMultiECC = exsistMultiECCsub != -1 ? exsistMultiECCsub : normalECC;
-    System.out.println("exsistMultiECC = " + exsistMultiECC);
-    System.out.println();
+    // ファイル書き込み
+    exportCsv(fileName, eccArray);
+    System.out.println("ファイル出力しました");
+  }
 
-    System.out.println("提案手法の誤り訂正能力");
-    double proposedECC = calcProposedECC(ecb, numTotalCodewords);
-    System.out.println("proposedECC = " + proposedECC);
-    System.out.println();
+  // ファイル書き込み
+  public static void exportCsv(String fileName, double[][] eccArray){
 
+    try {
+      File file = new File(fileName);
+      FileWriter filewriter = new FileWriter(file);
+      BufferedWriter bw = new BufferedWriter(filewriter);
+
+      if (eccArray.length == 4) {
+        // bw.write("埋め込む情報,提案手法,従来手法,既存手法(単一),既存手法(複数)");
+        bw.write("k',proposed,normal,exsist(single),exsist(multi)");
+      } else {
+        // bw.write("埋め込む情報,提案手法,従来手法,既存手法(複数)");
+        bw.write("k',proposed,normal,exsist(multi)");
+      }
+      bw.newLine();
+
+      for (int i = 0; i < eccArray[0].length; i++) {
+        bw.write(String.valueOf(i + 1));
+        bw.write(",");
+        for (int j = 0; j < eccArray.length; j++) {
+          bw.write(String.valueOf(eccArray[j][i]));
+          if(j != eccArray.length - 1) {
+            bw.write(",");
+          }
+        }
+        bw.newLine();
+      }
+      bw.close();
+
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
   }
 
   // 提案手法の誤り訂正能力を返すメソッド
@@ -63,14 +137,14 @@ public class OutputECC {
     double ecc = 0;
     for (NewVersion.NewECB block : ecb) {
       // デバッグ用
-      System.out.println(block.getCount() + "×(" + block.getCodewords() + "," + block.getDataCodewords() + ")" );
+      // System.out.println(block.getCount() + "×(" + block.getCodewords() + "," + block.getDataCodewords() + ")" );
       // 誤り訂正可能な数
       int t = (block.getCodewords() - block.getDataCodewords()) / 2;
       // 誤り訂正可能な数の総和
       ecc += block.getCount() * t ;
     }
     // 誤り訂正可能な数の総和 / 符号長の総和
-    ecc /= (double) numTotalCodewords;
+    ecc = ecc / (double) numTotalCodewords * 100;
     return ecc;
   }
 
@@ -80,10 +154,10 @@ public class OutputECC {
   public static double calcExsistSingleECC(int kp, int numTotalCodewords) {
     double ecc = 0;
     // デバッグ用
-    System.out.println("(" + numTotalCodewords + "," + kp + ")");
+    // System.out.println("(" + numTotalCodewords + "," + kp + ")");
     // 誤り訂正可能な数
     int t = (numTotalCodewords - kp) / 2;
-    ecc = t / (double) numTotalCodewords;
+    ecc = t / (double) numTotalCodewords * 100;
     return ecc;
   }
 
@@ -118,9 +192,9 @@ public class OutputECC {
 
         for (int i = 0; i < nkParam.get(0).size(); i++) {
           // デバッグ用
-          System.out.print(nkParam.get(0).get(i) + "×(");
-          System.out.print(nkParam.get(1).get(i) + ",");
-          System.out.println(nkParam.get(2).get(i) + ")");
+//          System.out.print(nkParam.get(0).get(i) + "×(");
+//          System.out.print(nkParam.get(1).get(i) + ",");
+//          System.out.println(nkParam.get(2).get(i) + ")");
           // 誤り訂正可能な数を求める
           int t = (nkParam.get(1).get(i) - nkParam.get(2).get(i)) / 2;
           // 誤り訂正可能な数の総和
@@ -130,9 +204,9 @@ public class OutputECC {
         ecc /= (double) (kp + p);
       } else {
         // デバッグ用
-        System.out.println("(" + (kp + p) + "," + p + ")");
+        // System.out.println("(" + (kp + p) + "," + p + ")");
         // そのまま求める
-        ecc = (p / 2) / (double) (kp + p);
+        ecc = (p / 2) / (double) (kp + p) * 100;
       }
     }
 
@@ -148,12 +222,12 @@ public class OutputECC {
       // 各RSブロックごとの符号長
       int n = ecb.getDataCodewords() + ecBlocks.getECCodewordsPerBlock();
       // デバッグ用
-      System.out.println(ecb.getCount() + "×(" + n + "," + ecb.getDataCodewords() + ")");
+      // System.out.println(ecb.getCount() + "×(" + n + "," + ecb.getDataCodewords() + ")");
       // 誤り訂正可能な数の総和
       ecc += ecb.getCount() * t;
     }
     // 誤り訂正可能な数の総和 / 符号長の総和
-    ecc /= (double) numTotalCodewords;
+    ecc = ecc / (double) numTotalCodewords * 100;
     return ecc;
   }
 
